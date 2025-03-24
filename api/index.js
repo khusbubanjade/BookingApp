99c8101d99499de8dcbd0657c958b6def930967a
khusbubanjade <khusbubanjade7@gmail.com>
2025-03-24 10:38:42 +0545

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,17 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
 const app = express()
 dotenv.config()
 
 const connect = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO);
-        console.log("connected to mongoDB")
+        console.log("connected to mongoDB.")
     } catch (error) {
-        throw error
+        throw error;
     }
 };
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!")
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected!")
+})
+
+// middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
     
 app.listen(8800, ()=>{
     connect()
